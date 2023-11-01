be4298b8f102f23f9ba6e5fd4f6ad7a2a31e34d7
Kevin Patto <kevp2010@gmail.com>
2023-10-31 21:45:51 -0500

    removed postroutes (unnecessary)

--- controllers/view/index.js
+++ controllers/view/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
-const postRoutes = require('./postRoutes');
 
 // PATH /
 
 router.use('/', homeRoutes); // ROUTE TO /
-router.use('/profile', postRoutes); // ROUTE TO /post
 
 module.exports = router;
