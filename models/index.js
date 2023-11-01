e63ddbc95bbd5a1d7869fb88f18a73eb8d9e2d1c
Kevin Patto <kevp2010@gmail.com>
2023-10-31 19:37:11 -0500

    added relationships and other models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,35 @@
 const User = require('./User');
-// const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+// one to many user to post
+User.hasMany(Post, {
+	foreignKey: 'user_id',
+	onDelete: 'CASCADE'
+});
 
+Post.belongsTo(User, {
+	foreignKey: 'user_id'
+});
 
-module.exports = { User };
+// one to many user to comment
+User.hasMany(Comment, {
+	foreignKey: 'user_id',
+	onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+	foreignKey: 'user_id'
+});
+
+// one to many post to comment
+Post.hasMany(Comment, {
+	foreignKey: 'post_id',
+	onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+	foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
