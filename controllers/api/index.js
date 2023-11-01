ffa075997180182a788351076e87c0a9f4e6a11c
Kevin Patto <kevp2010@gmail.com>
2023-10-31 21:43:20 -0500

    added post and comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 // SERVER PATH /api
 
 router.use('/users', userRoutes); // ROUTE TO /api/users
+router.use('/posts', postRoutes); // ROUTE TO /api/posts
+router.use('/comments', commentRoutes); // ROUTE TO /api/comments
 
 module.exports = router;
