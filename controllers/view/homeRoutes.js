34cfea1bc98a670e185e0f52985bfb9d3d5f2673
Kevin Patto <kevp2010@gmail.com>
2023-10-31 19:36:00 -0500

    moved homeroutes

---
+++ controllers/view/homeRoutes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const auth = require('../../utils/auth'); // auth middleware helper
+// SERVER PATH /
+
+router.get('/', async (req, res) => {
+	try {
+		// get all posts and sort by date_created descending (most recent to least recent (i hope))
+		const postsData = await Post.findAll({
+			order: [
+				['date_created', 'DESC'],
+			],
+		});
+
+		// serialize data
+		const posts = postsData.map((post) => post.get({ plain: true }));
+
+		res.render('homepage', {
+			posts,
+			logged_in: req.session.logged_in
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/login', (req, res) => {
+	if (req.session.logged_in) {
+		res.redirect('/profile');
+		return;
+	}
+
+	res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+	if (req.session.logged_in) { // why are you trying to sign up if you're already logged in? dumbo
+		res.redirect('/profile');
+		return;
+	}
+
+	res.render('signup');
+});
+
+// page for own profile
+router.get('/profile', auth, async (req, res) => {
+	try {
+		const userData = await User.findByPk(req.session.user_id, {
+			attributes: { exclude: ['password'] },
+			include: [{ model: Post }],
+		});
+
+		const user = userData.get({ plain: true });
+
+		res.render('dashBoard', {
+			...user,
+			logged_in: true
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+module.exports = router;
